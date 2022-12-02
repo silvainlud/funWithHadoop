7c7407c65019a101f285daf36261e93cb923ad8a
thibaud <thibaud@BAT-PC.home>
2022-12-02 11:05:36 +0100

    initial commit

---
+++ src/main/java/edu/reduce/map/fun/App.java
@@ -0,0 +1,32 @@
+package edu.reduce.map.fun;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(final String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        final Job job = configureJob("funWithMapReduce");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.waitForCompletion(true);
+    }
+
+    private static Job configureJob(final String jobName) throws IOException {
+        Job job = Job.getInstance(new Configuration(), jobName);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        return job;
+    }
+}
---
+++ src/main/java/edu/reduce/map/fun/Reduce.java
@@ -0,0 +1,14 @@
+package edu.reduce.map.fun;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
+}
